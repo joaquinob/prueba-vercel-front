61e45337adf894785b7473a38c8387787d132f01
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-06-21 20:23:45 +0200

    cambio ruta vehicles y users

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { User } from '../interfaces/user';
 export class AuthService {
 
   user: User|null = null
-  url: string = "https://prueba-back-vercel.vercel.app/api/users"
+  url: string = "https://backend-supercars-vercel.vercel.app/api/users"
 
 
   constructor(private http : HttpClient, private cookieService: CookieService) {
